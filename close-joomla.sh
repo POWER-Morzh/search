84d5201863d814acac0be71287a7b595255be72f
POWER-Morzh <denys.korzh@gmail.com>
2014-08-28 14:37:23 +0200

    Premissions...

--- close-joomla.sh
+++ close-joomla.sh
@@ -4,5 +4,5 @@ echo "That's better! Secure Again!"
 chown -R -f www-data:www-data .
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
-find . -name 'index.php' -exec chmod 440 {} \;
-chmod -R 440 ./configuration.php
+#find . -name 'index.php' -exec chmod 440 {} \;
+#chmod -R 440 ./configuration.php
